2e49d274a649a4a8c2d5dfbd6c384488640826ac
networkandy <red2animation@gmail.com>
2019-02-11 20:35:07 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+
+
+
+//Sticky menu background
+window.addEventListener('scroll', function(){
+    if(window.scrollY > 150){
+        document.querySelector('#navbar').style.opacity = 0.9;
+
+    } else {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+
+
+//Smooth Scrolling
+$('#navbar a').on('click', function(e){
+    if(this.hash !== ''){
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+});
